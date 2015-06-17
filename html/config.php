eb27871a34748c65d289ae0789f4eee47d7b0db6
Laura Moraes <lmoraes@cncflora.net>
2015-06-17 11:46:10 -0300

    Changing configurations and file location to use docker-compose as default

--- html/config.php
+++ html/config.php
@@ -1,9 +1,9 @@
 <?php
 
-include_once __DIR__.'/vendor/autoload.php';
+include_once __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
-$raw = Yaml::parse(file_get_contents(__DIR__."/config.yml"));
+$raw = Yaml::parse(file_get_contents(__DIR__."/../config.yml"));
 
 $env = getenv("PHP_ENV");
 if($env == null) {
@@ -16,19 +16,22 @@ if(isset($_SERVER) && isset($_SERVER['HTTP_HOST'])) {
     }
 }
 
-$data["ENV"] = $env;
-$data["TEST"]=($env=='test');
-
-//$couchdb_ip=getenv("COUCHDB_PORT_5984_TCP_ADDR");
-//if($couchdb_ip) {
-  //define("COUCHDB","http://".$couchdb_ip.":5984");
-//}
+$data["PHP_ENV"] = $env;
 
 $array = $raw[$env];
+
+foreach($array as $key=>$value) {
+    preg_match_all('/\$([a-zA-Z]+)/',$value,$reg);
+    if(count($reg[0]) >= 1) {
+      $e = getenv($reg[1][0]);
+      $data[strtoupper($key)] = str_replace($reg[0][0],$e,$value);
+    } else {
+      $data[strtoupper($key)] = $value;
+    }
+}
 
 foreach($array as $k=>$v) {
   if(!defined(strtoupper($k))) {
     define(strtoupper($k),$v);
   }
 }
-
